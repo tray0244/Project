c1c81ccea67063fb9fecec26303351ac396a3b11
tray0244 <tray024400@gmail.com>
2025-12-16 18:48:34 +0900

    [feat] User Service 기본 파일 푸시

---
+++ store-servoce/src/main/java/com/delivery/storeservic/StoreServoceApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.storeservic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServoceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreServoceApplication.class, args);
+	}
+
+}
---
+++ store-servoce/src/test/java/com/delivery/storeservic/StoreServoceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.storeservic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
